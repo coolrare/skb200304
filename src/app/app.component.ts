c60c2e57174ddcb36964586af93e938009e6a9d0
Will <doggy.huang@gmail.com>
2020-03-04 14:12:55 +0800

    示範從 Angular 載入第三方 jQuery 函式庫的寫法
    1. 從 index.html 載入第三方函式庫
    2. 透過 window['jQuery'] 存取全域屬性
    3. 使用 // tslint:disable-next-line: 取消特定一行的 tslint 檢查

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'skb200304';
+  constructor() {
+    // tslint:disable-next-line: no-string-literal
+    window['jQuery'](() => {
+      console.log('ok');
+    });
+  }
 }
